08eb52655c406675fc6536bf7276dbbf88dada22
Joe-luke <88790233+Joe-luke@users.noreply.github.com>
2022-06-10 15:18:38 +0300

    update 10/6/22

--- Streamlit_BreastCancer.py
+++ Streamlit_BreastCancer.py
@@ -6,12 +6,12 @@ st.title("Breast Cancer Detection App")
 
 def get_predictions(radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean,concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst,
 texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst):
-    url = "https://breast-cancer-detection-system.herokuapp.com/predict?radius_mean={radius_mean}&texture_mean={texture_mean}&perimeter_mean={perimeter_mean}&area_mean={area_mean}&smoothness_mean={smoothness_mean}&compactness_mean={compactness_mean}&concavity_mean={concavity_mean}&concave_points_mean={concave_points_mean}&symmetry_mean={symmetry_mean}&fractal_dimension_mean={fractal_dimension_mean}&radius_se={radius_se}&texture_se={texture_se}&perimeter_se={perimeter_se}&area_se={area_se}&smoothness_se={smoothness_se}&compactness_se={compactness_se}&concavity_se={concavity_se}&concave_points_se={concave_points_se}&symmetry_se={symmetry_se}&fractal_dimension_se={fractal_dimension_se}&radius_worst={radius_worst}&texture_worst={texture_worst}&perimeter_worst={perimeter_worst}&area_worst={area_worst}&smoothness_worst={smoothness_worst}&compactness_worst={compactness_worst}&concavity_worst={concavity_worst}&concave_points_worst={concave_points_worst}&symmetry_worst={symmetry_worst}&fractal_dimension_worst={fractal_dimension_worst}" \
-    .format(radius_mean=radius_mean, texture_mean=texture_mean, perimeter_mean=perimeter_mean, area_mean=area_mean, smoothness_mean=smoothness_mean, compactness_mean=compactness_mean, concavity_mean=concavity_mean, concave_points_mean=concave_points_mean, symmetry_mean=symmetry_mean, fractal_dimension_mean=fractal_dimension_mean, radius_se=radius_se, texture_se=texture_se, perimeter_se=perimeter_se, area_se=area_se, smoothness_se=smoothness_se, compactness_se=compactness_se, concavity_se=concavity_se, concave_points_se=concave_points_se, symmetry_se=symmetry_se, fractal_dimension_se=fractal_dimension_se, radius_worst=radius_worst, texture_worst=texture_worst, perimeter_worst=perimeter_worst, area_worst=area_worst, smoothness_worst=smoothness_worst, compactness_worst=compactness_worst, concavity_worst=concavity_worst, concave_points_worst=concave_points_worst, symmetry_worst=symmetry_worst, fractal_dimension_worst=fractal_dimension_worst)
+    url = f"https://breast-cancer-detection-system.herokuapp.com/predict?radius_mean={radius_mean}&texture_mean={texture_mean}&perimeter_mean={perimeter_mean}&area_mean={area_mean}&smoothness_mean={smoothness_mean}&compactness_mean={compactness_mean}&concavity_mean={concavity_mean}&concave_points_mean={concave_points_mean}&symmetry_mean={symmetry_mean}&fractal_dimension_mean={fractal_dimension_mean}&radius_se={radius_se}&texture_se={texture_se}&perimeter_se={perimeter_se}&area_se={area_se}&smoothness_se={smoothness_se}&compactness_se={compactness_se}&concavity_se={concavity_se}&concave_points_se={concave_points_se}&symmetry_se={symmetry_se}&fractal_dimension_se={fractal_dimension_se}&radius_worst={radius_worst}&texture_worst={texture_worst}&perimeter_worst={perimeter_worst}&area_worst={area_worst}&smoothness_worst={smoothness_worst}&compactness_worst={compactness_worst}&concavity_worst={concavity_worst}&concave_points_worst={concave_points_worst}&symmetry_worst={symmetry_worst}&fractal_dimension_worst={fractal_dimension_worst}"
     response = requests.post(url)
     json_response = response.json()
     diagnosis = json_response["prediction"]
     return diagnosis
+
 
 radius_mean = st.number_input("Enter radius mean")
 texture_mean = st.number_input("Enter texture mean")
@@ -49,5 +49,16 @@ result = ""
 # when 'Predict' is clicked, make the prediction and store it
 
 if st.button("Predict"):
-    result = get_predictions(radius_mean=radius_mean, texture_mean=texture_mean, perimeter_mean=perimeter_mean, area_mean=area_mean, smoothness_mean=smoothness_mean, compactness_mean=compactness_mean, concavity_mean=concavity_mean, concave_points_mean=concave_points_mean, symmetry_mean=symmetry_mean, fractal_dimension_mean=fractal_dimension_mean, radius_se=radius_se, texture_se=texture_se, perimeter_se=perimeter_se, area_se=area_se, smoothness_se=smoothness_se, compactness_se=compactness_se, concavity_se=concavity_se, concave_points_se=concave_points_se, symmetry_se=symmetry_se, fractal_dimension_se=fractal_dimension_se, radius_worst=radius_worst, texture_worst=texture_worst, perimeter_worst=perimeter_worst, area_worst=area_worst, smoothness_worst=smoothness_worst, compactness_worst=compactness_worst, concavity_worst=concavity_worst, concave_points_worst=concave_points_worst, symmetry_worst=symmetry_worst, fractal_dimension_worst=fractal_dimension_worst)
+    result = get_predictions(radius_mean=radius_mean, texture_mean=texture_mean, perimeter_mean=perimeter_mean,
+                             area_mean=area_mean, smoothness_mean=smoothness_mean, compactness_mean=compactness_mean,
+                             concavity_mean=concavity_mean, concave_points_mean=concave_points_mean,
+                             symmetry_mean=symmetry_mean, fractal_dimension_mean=fractal_dimension_mean,
+                             radius_se=radius_se, texture_se=texture_se, perimeter_se=perimeter_se, area_se=area_se,
+                             smoothness_se=smoothness_se, compactness_se=compactness_se, concavity_se=concavity_se,
+                             concave_points_se=concave_points_se, symmetry_se=symmetry_se,
+                             fractal_dimension_se=fractal_dimension_se, radius_worst=radius_worst,
+                             texture_worst=texture_worst, perimeter_worst=perimeter_worst, area_worst=area_worst,
+                             smoothness_worst=smoothness_worst, compactness_worst=compactness_worst,
+                             concavity_worst=concavity_worst, concave_points_worst=concave_points_worst,
+                             symmetry_worst=symmetry_worst, fractal_dimension_worst=fractal_dimension_worst)
     st.success(f"Breast Cancer  {result}")
